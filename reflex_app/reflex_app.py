24bceedbb3c0d112dd391fd10c7d07be84de490a
abhisek11 <raizadaabhi11@gmail.com>
2024-10-03 03:07:11 +0530

    "Removed navbar_link and navbar functions, and modified base_page function to import from .ui.base"

--- reflex_app/reflex_app.py
+++ reflex_app/reflex_app.py
@@ -3,7 +3,7 @@
 import reflex as rx
 
 from rxconfig import config
-
+from .ui.base import base_page
 
 class State(rx.State):
     """The app state."""
@@ -15,105 +15,7 @@ class State(rx.State):
     def did_click(self):
         print("The event on click")
 
-def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(
-        rx.text(text, size="4", weight="medium"), href=url
-    )
 
-
-def navbar() -> rx.Component:
-    return rx.box(
-        rx.desktop_only(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="/logo.jpg",
-                        width="2.25em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="7", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.hstack(
-                    navbar_link("Home", "/#"),
-                    navbar_link("About", "/#"),
-                    navbar_link("Pricing", "/#"),
-                    navbar_link("Contact", "/#"),
-                    spacing="5",
-                ),
-                rx.hstack(
-                    rx.button(
-                        "Sign Up",
-                        size="3",
-                        variant="outline",
-                    ),
-                    rx.button("Log In", size="3"),
-                    spacing="4",
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        rx.mobile_and_tablet(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="/logo.jpg",
-                        width="2em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="6", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.menu.root(
-                    rx.menu.trigger(
-                        rx.icon("menu", size=30)
-                    ),
-                    rx.menu.content(
-                        rx.menu.item("Home"),
-                        rx.menu.item("About"),
-                        rx.menu.item("Pricing"),
-                        rx.menu.item("Contact"),
-                        rx.menu.separator(),
-                        rx.menu.item("Log in"),
-                        rx.menu.item("Sign up"),
-                    ),
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        bg=rx.color("accent", 3),
-        padding="1em",
-        # position="fixed",
-        # top="0px",
-        # z_index="5",
-        width="100%",
-    )
-def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
-    if not isinstance(child, rx.Component):
-        child = rx.heading("This is not a valid child! element here", size="6"),
-    
-    if hide_navbar:
-        return rx.container(
-        child,
-        rx.color_mode.button(position="top-right"),
-        rx.logo(),
-    )
-    return rx.container(
-        navbar(),
-        child,
-        rx.color_mode.button(position="top-right"),
-        rx.logo(),
-    )
 
 def index() -> rx.Component:
     # Welcome Page (Index)
